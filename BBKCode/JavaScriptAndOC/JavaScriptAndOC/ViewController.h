cdc6ae71412dca57b55a60613368598fe501c782
zxx_mbp <zxx_mbp@zxx-mbpdemacbook-pro.local>
2017-05-04 11:36:27 +0800

    使用注入模型《OCModel》 的方法来进行交互

--- BBKCode/JavaScriptAndOC/JavaScriptAndOC/ViewController.h
+++ BBKCode/JavaScriptAndOC/JavaScriptAndOC/ViewController.h
@@ -7,6 +7,64 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <JavaScriptCore/JavaScriptCore.h>
+
+@protocol JavaScriptObjctiveCDelegate <JSExport>
+
+// JS调用此方法来调用OC的系统相册方法
+- (void)callSystemCamera;
+
+/**
+ 在JS中调用时函数名应该为showAlertMsg(arg1,arg2)
+ 这里是只有两个参数
+ */
+- (void)showAlert:(NSString*)title msg:(NSString*)msg;
+// 通过JSON传过来
+- (void)callWithDict:(NSDictionary*)params;
+//JS调用OC，然后在OC中通过调用JS方法来传给JS
+- (void)jsCallObjcAndObjcCallJsWithDict:(NSDictionary*)params;
+@end
+
+// 此模型用于注入JS的模型，这样就可以通过模型来调用方法。
+
+@interface HYBJsObjCModel : NSObject<JavaScriptObjctiveCDelegate>
+
+@property (nonatomic, weak) JSContext *jsContext;
+@property (nonatomic, weak) UIWebView *webView;
+
+@end
+
+@implementation HYBJsObjCModel
+
+- (void)callWithDict:(NSDictionary *)params {
+    NSLog(@"Js调用了OC的方法，参数为：%@",params);
+}
+
+- (void)callSystemCamera {
+    NSLog(@"Js调用了OC的方法，调用系统相册");;
+    //JS调用OC后，又通过OC调用JS，但是这个是没有参数传递的
+    JSValue *jsFunc = self.jsContext[@"jsFunc"];
+    [jsFunc callWithArguments:nil];
+}
+
+- (void)jsCallObjcAndObjcCallJsWithDict:(NSDictionary *)params {
+    NSLog(@"jsCallObjcAndObjcCallJsWithDict was called, params is %@", params);
+    
+    // 调用JS的方法
+    JSValue *jsParamFunc = self.jsContext[@"jsParamFunc"];
+    [jsParamFunc callWithArguments:@[@{@"age": @10, @"name": @"lili", @"height": @158}]];
+}
+
+- (void)showAlert:(NSString *)title msg:(NSString *)msg {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIAlertView * alert = [[UIAlertView alloc]initWithTitle:title message:msg delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil , nil];
+        [alert show];
+        
+    });
+
+}
+
+@end
 
 @interface ViewController : UIViewController
 
--- BBKCode/JavaScriptAndOC/JavaScriptAndOC/ViewController.m
+++ BBKCode/JavaScriptAndOC/JavaScriptAndOC/ViewController.m
@@ -8,7 +8,19 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+
+/**
+ ObjC与JS交互方式
+ 
+ 通过JSContext，我们有两种调用JS代码的方法：
+ 
+ 1、直接调用JS代码
+ 2、在ObjC中通过JSContext注入模型，然后调用模型的方法
+ */
+@interface ViewController ()<UIWebViewDelegate>
+
+@property (nonatomic, strong) UIWebView *webView;
+@property (nonatomic, strong) JSContext *jsContext;
 
 @end
 
@@ -16,7 +28,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //一、直接调用JS代码
+   /*
+    // 一个JSContext对象，就类似于Js中的window，
+    // 只需要创建一次即可。
+    self.jsContext = [[JSContext alloc] init];
+    
+    //  jscontext可以直接执行JS代码。
+    [self.jsContext evaluateScript:@"var num = 10"];
+    [self.jsContext evaluateScript:@"var squareFunc = function(value) { return value * 2 }"];
+    // 计算正方形的面积
+    JSValue *square = [self.jsContext evaluateScript:@"squareFunc(num)"];
+    
+    // 也可以通过下标的方式获取到方法
+    JSValue *squareFunc = self.jsContext[@"squareFunc"];
+    JSValue *value = [squareFunc callWithArguments:@[@"20"]];
+    NSLog(@"%@", square.toNumber);
+    NSLog(@"%@", value.toNumber);
+    */
+    
+    [self.view addSubview:self.webView];
 }
 
 
@@ -25,5 +57,35 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (UIWebView *)webView {
+    if (!_webView) {
+        _webView = [[UIWebView alloc]initWithFrame:self.view.bounds];
+        _webView.scalesPageToFit = YES;
+        NSURL *url = [[NSBundle mainBundle] URLForResource:@"test" withExtension:@"html"];
+        NSURLRequest* request = [NSURLRequest requestWithURL:url];
+        [_webView loadRequest:request];
+        _webView.delegate = self;
+    }
+    return _webView;
+}
 
+#pragma mark - UIWebViewDelegate
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    //二、通过注入模型的方式交互
+    self.jsContext = [webView valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
+    //通过模型调用方法，这种方法更好些
+    HYBJsObjCModel* model = [[HYBJsObjCModel alloc]init];
+    self.jsContext[@"OCModel"] = model;
+    model.jsContext = self.jsContext;
+    model.webView = self.webView;
+    
+    self.jsContext.exceptionHandler = ^(JSContext *context, JSValue *exception) {
+        context.exception = exception;
+        NSLog(@"异常信息：%@",exception);
+    };
+}
+
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    
+}
 @end
